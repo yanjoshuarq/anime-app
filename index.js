f704994bccd78740cb9c4b87efaa72fb2cc9d345
Victoria, Dev Theme <clearcm2011@gmail.com>
2022-07-12 17:43:48 -0300

    <what you have updated>

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+// npm install express
+const app = express()
+const path = require("path")
+
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "/views"))
+// npm install ejs
+
+// this starter pack uses ejs as a display library for html you can use the library you like
+
+app.use(express.static(path.join(__dirname, "/public")))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.get("/", (req, res) => {
+   res.render("index")
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+   console.log(`working on port ${port}`)
+})
