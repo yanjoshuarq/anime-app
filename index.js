394a3d337868c6afabee6675d3f0c28bed96b604
KiritoGalaxy <110548299+KiritoGalaxyS2@users.noreply.github.com>
2022-08-03 13:13:37 -0300

    add 17 anime

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const path = require("path")
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "/views"))
 // npm install ejs
-
 // this starter pack uses ejs as a display library for html you can use the library you like
 
 app.use(express.static(path.join(__dirname, "/public")))
@@ -18,6 +17,9 @@ app.get("/", (req, res) => {
 })
 app.get("/downloads", (req, res) => {
    res.render("downloads")
+})
+app.get("/staff", (req, res) => {
+   res.render("staff")
 })
 
 const port = process.env.PORT || 3000
